224930249f204627d94d28b968d7995319621838
vishal-9368 <vshalsha@1234@gmail.com>
2024-05-22 13:36:16 +0530

    fid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+import "./app.css";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff, faBell } from "@fortawesome/free-solid-svg-icons";
+import { Locations } from "./Route";
+export default function App() {
+  const [user, setuser] = useState();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  function data() {
+    const us = localStorage.getItem("user");
+    const g = JSON.parse(us);
+    console.log(g);
+    setuser(g);
+  }
+  function handleLogout() {
+    const us = localStorage.removeItem("user");
+    window.location.href = "/login";
+  }
+  useEffect(() => {
+    data();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <Sidebark/>
+    <>
+      <header
+        className={`${user == null ? "r" : ""}`}
+        style={{
+          height: "60px",
+          backgroundColor: "#76b11d",
+          position: "fixed",
+          top: "0",
+          left: "0",
+          width: "100vw",
+          zIndex: "1000",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "0 30px",
+          gap: "10px",
+          border: "2px solid black",
+        }}
+      >
+        <h1
+          style={{
+            alignSelf: "left",
+            fontFamily: "Dancing Script, cursive",
+            fontSize: "2.5rem",
+          }}
         >
-          Learn React
-        </a>
+          OFF DAYS INN
+        </h1>
+        <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          <FontAwesomeIcon icon={faBell} style={{ color: "#ffffff" }} />
+          <div className="dropdown">
+            <button style={{ borderRadius: "10px" }} onClick={handleOpen}>
+              <FontAwesomeIcon icon={faPowerOff} style={{ color: "#ffffff" }} />
+            </button>
+            {open ? (
+              <ul className="menu1">
+                <li className="menu1-item">
+                  <button>Change Password</button>
+                </li>
+                <li className="menu1-item">
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
+            ) : null}
+          </div>
+        </div>
       </header>
-    </div>
+      <div
+        style={
+          localStorage.getItem("user") != null
+            ? { position: "relative", top: "60px" }
+            : {}
+        }
+      >
+        <Locations user={user} />
+      </div>
+    </>
   );
 }
-
-export default App;
